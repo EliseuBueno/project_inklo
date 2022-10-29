e201308dffd6b067b5f36bcaeaf35379ee6a14a7
Eliseu Bueno <sturarobuenoeliseu@gmail.com>
2022-10-29 15:15:24 -0300

    Feat: Importação de componente Navbar no projeto

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <template>
+    <nav-bar/>
     <router-view/>
 </template>
 
 <script>
+import NavBar from './components/layouts/NavBar.vue'
     export default {
+  components: { NavBar },
         name: ('App')
     }
 </script>
