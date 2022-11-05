2c2c04906cb206c04924a70d2041fed9e716ef6c
Eliseu Bueno <sturarobuenoeliseu@gmail.com>
2022-11-04 22:53:53 -0300

    Feat: Criação de Controller para receber os dados e gerar o arquivo json

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\SaveController;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('salvar-local', function(){
+//     echo 'Teste';
+// });
+Route::post('/salvar-local', [SaveController::class, 'store']);
