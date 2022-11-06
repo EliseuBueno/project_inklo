be36f6ba0f6d11cfcabedbd8db70cb0a2456b7a4
Eliseu Bueno <sturarobuenoeliseu@gmail.com>
2022-11-06 18:24:19 -0300

    Feat: Adicionado campo de pesquisa para página Home, ajustado propriedades composition api

--- frontend/src/components/layouts/NavbarComponent.vue
+++ frontend/src/components/layouts/NavbarComponent.vue
@@ -11,10 +11,6 @@
                         <router-link to="/" class="nav-link active" aria-current="page">Home</router-link>
                     </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-success" type="submit">Search</button>
-                </form>
             </div>
         </div>
     </nav>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,34 @@
 <template>
     <div class="container">
-        <h1 class="text-center">Lista de Usuários Github</h1>
-        <hr><br>
-        <div class="row row-cols-2 row-cols-md-2 g-4">
-            <card-component
-                v-for="(profile, index) in profiles"
-                :key="index"
-                :img="profile.avatar_url"
-                :user= "profile.name"
-                :registration_dt= "profile.created_at"
-                :public_repos= "profile.public_repos"
-                :login= "profile.login"
-                :alt= "`Avatar_${profile.login}`"
-            />
-        </div>
+        <form>
+            
+            <h1 class="text-center">Lista de Usuários GitHub</h1>
+            <div class="row">
+                <div class="col-md-9"></div>
+                <div class="col-md-3">
+                    <input 
+                        class="form-control me-2" 
+                        type="search" 
+                        placeholder="Busca por Nome ou Usuário" 
+                        aria-label="Search"
+                        v-model="search"
+                    >
+                </div>
+            </div>
+            <hr><br>
+            <div class="row row-cols-2 row-cols-md-2 g-4">
+                <card-component
+                    v-for="(profile, index) in itensFilter"
+                    :key="index"
+                    :img="profile.avatar_url"
+                    :user= "profile.name"
+                    :registration_dt= "profile.created_at"
+                    :public_repos= "profile.public_repos"
+                    :login= "profile.login"
+                    :alt= "`Avatar_${profile.login}`"
+                />
+            </div>
+        </form>
     </div>
 </template>
 
@@ -22,7 +37,7 @@
     import {format} from "date-fns"
     import CardComponent from '../components/CardComponent.vue'
     import { userStore } from '../store/user';
-    import { ref } from "vue";
+    import { ref, computed, onMounted } from "vue";
 
     export default {
         components: { CardComponent },
@@ -30,8 +45,9 @@
         setup() {
 
             const profiles = ref([])
+            const search = ref('')
 
-            const getProfiles = () => {
+            const getProfiles = onMounted(() => {
                 const store = userStore()
                 const listUsers = store.listUsers
                 Array.from(listUsers).forEach(use =>
@@ -45,7 +61,7 @@
                             console.log(error);
                         })
                 )
-            }
+            })
 
             const detailUser = (login) => {
                 fetch(`https://api.github.com/users/${login}`, {
@@ -54,13 +70,23 @@
                 this.getProfiles();
             }
 
+            const itensFilter = computed(() => {
+                let valores = []
+                valores = profiles.value.filter((profile) => {
+                    return (
+                        profile.name.toLowerCase().indexOf(search.value.toLowerCase()) > -1 ||
+                        profile.login.toLowerCase().indexOf(search.value.toLowerCase()) > -1
+                    )
+                })
+                return valores
+            })
+
             return {
                 profiles,
-                getProfiles
+                getProfiles,
+                search,
+                itensFilter
             }
-        },
-        mounted (){
-            this.getProfiles()
         }
     }
 </script>
